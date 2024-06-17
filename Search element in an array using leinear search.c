a7af909a0fc9a810638d6ebf3dca27e440aeed99
KPrathiksha <155699818+KPrathiksha@users.noreply.github.com>
2024-06-17 19:42:06 +0530

    Update Search element in an array using leinear search.c

--- Search element in an array using leinear search.c
+++ Search element in an array using leinear search.c
@@ -1,26 +1,57 @@
-// C code to linearly search x in arr[].
-
 #include <stdio.h>
 
-int search(int arr[], int N, int x)
-{
-    for (int i = 0; i < N; i++)
-        if (arr[i] == x)
-            return i;
-    return -1;
-}
+void main()
 
-// Driver code
-int main(void)
-{
-    int arr[] = { 2, 3, 4, 10, 40 };
-    int x = 10;
-    int N = sizeof(arr) / sizeof(arr[0]);
+{  
 
-    // Function call
-    int result = search(arr, N, x);
-    (result == -1)
-        ? printf("Element is not present in array")
-        : printf("Element is present at index %d", result);
-    return 0;
+    int num;
+
+    int i,  key, element_found = 0;
+
+    printf("Enter number of elements you would like to take as input: ");
+
+    scanf("%d", &num);
+
+    int arr[num];
+
+    printf("\nEnter all the elements of your choice:");
+
+    for (i = 0; i < num; i++)
+
+    {
+
+        scanf("%d", &arr[i]);
+
+    }
+
+    printf("\nEnter the key element that you would like to be searched: ");
+
+    scanf("%d", &key);
+
+    /*  Linear search starts */
+
+    for (i = 0; i < num ; i++)
+
+    {
+
+        if (key == arr[i] )
+
+        {
+
+            element_found = 1;
+
+            break;
+
+        }
+
+    }
+
+    if (element_found == 1)
+
+        printf("we got the element at index %d",i+1);
+
+    else
+
+        printf("we haven’t got element at any index in the array\n");
+
 }
